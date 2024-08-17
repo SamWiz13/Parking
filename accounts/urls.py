008d3b98ce16e89dc7a0e6c49f485a1cc3b2321a
SamWiz13 <samwith75@gmail.com>
2024-08-17 22:44:03 +0500

    check authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import register_user, user_login, user_logout
+from .views import register_user, user_login, user_logout,check_authentication
 
 urlpatterns = [
     path('register/', register_user, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
+    path('check/', check_authentication, name='check'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -17,7 +17,7 @@ def register_user(request):
         if serializer.is_valid():
             user = serializer.save()
             token, _ = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key, "User":serializer.data }, status=status.HTTP_201_CREATED)
+            return Response({'token': token.key, "user":serializer.data}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -39,7 +39,7 @@ def user_login(request):
 
         if user:
             token, _ = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key, "username": user.username, "password": user.password}, status=status.HTTP_200_OK)
+            return Response({'token': token.key, "username": user.username, "email": user.email}, status=status.HTTP_200_OK)
         return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
         
 @api_view(['POST'])
@@ -52,3 +52,22 @@ def user_logout(request):
             return Response({'message': 'Successfully logged out.'}, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+
+@api_view(['POST'])
+def check_authentication(request):
+    if request.method == 'POST':
+        token = request.data.get('token')
+        if token:
+            try:
+                token_obj = Token.objects.get(key=token)
+                if token_obj:
+                    user = token_obj.user
+                    return Response({'message': 'User is authenticated', 'username': user.username, 'email': user.email}, status=status.HTTP_200_OK)
+                else:
+                    return Response({'error': 'Token is invalid'}, status=status.HTTP_401_UNAUTHORIZED)
+            except Token.DoesNotExist:
+                return Response({'error': 'Token is invalid'}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({'error': 'Token is required'}, status=status.HTTP_400_BAD_REQUEST)
